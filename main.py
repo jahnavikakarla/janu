dda4767becea9859049cc9ff2ff21c43224311f2
jahnavikakarla <jahnavikakarla394@gmail.com>
2022-09-08 14:05:33 +0530

    add print statement

---
+++ main.py
@@ -0,0 +1,12 @@
+def sum(a,b):
+    if a == 3 and b== 4:
+       return a+b
+    else:
+        return a-b
+print(sum(3,4))
+print(sum(7,8))
+def avg(a,b,c,d):
+    return a+b+c+d/4
+print(avg(6,7,8,9))
+
+
